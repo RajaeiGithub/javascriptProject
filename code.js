abea13d5d12af87f01618cc2a4f957c13a31f8cc
AliRajaee <rajaei.iran.3744@gmail.com>
2023-11-03 22:45:24 +0330

    V1

---
+++ code.js
@@ -0,0 +1,46 @@
+//scroll
+function visibel(){
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+    document.getElementById("head1").style.background = "none";
+    document.getElementById("mySidenav").style.top = 0;
+
+     }
+else
+{
+     document.getElementById("head1").style.backgroundImage = "linear-gradient(to top,#0553a0,#0884ff)";
+    document.getElementById("mySidenav").style.top = "87px";
+     
+}}
+
+
+
+//change size start up
+if(window.innerWidth >= 1000){
+  document.getElementById("mySidenav").style.width = "15%";
+  document.getElementById("grand").style.marginLeft = "250px";
+  document.getElementById("grand").style.width = "75%";
+  document.getElementById("fot").style.marginLeft = "250px";
+}
+else{
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("grand").style.width = "90%";
+  document.getElementById("grand").style.marginLeft= "0";
+  document.getElementById("fot").style.marginLeft = "0";
+
+}
+//change size
+function checksize(){
+     if(window.innerWidth >= 1000){
+     document.getElementById("mySidenav").style.width = "15%";
+     document.getElementById("grand").style.marginLeft = "250px";
+     document.getElementById("grand").style.width = "75%";
+     document.getElementById("fot").style.marginLeft = "250px";
+   }
+   
+   else{
+     document.getElementById("mySidenav").style.width = "0";
+     document.getElementById("grand").style.width = "90%";
+     document.getElementById("grand").style.marginLeft= "0";
+     document.getElementById("fot").style.marginLeft = "0";
+   }}
+   

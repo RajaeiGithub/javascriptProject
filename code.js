0240eaddd87283594ad0a197ba6e956f7854f7ce
AliRajaee <rajaei.iran.3744@gmail.com>
2024-01-01 00:56:23 +0330

    V7 E FE

--- code.js
+++ code.js
@@ -1,13 +1,18 @@
 // Added Elements with Javascript
-  //  document.body.removeEventListener( "scroll" , visibel); 
+  // document.body.removeEventListener( "scroll" , visibel); 
   // bod.addEventListener("scroll" , visibel);
 
 
 // variables
-var topbutton = document.getElementById("topbot");    
+    // Html
+var topbutton = document.getElementById("topbot");  
+var pointerButton = document.getElementById("pointerclose")
+    // Javascript  
 var saveloc = 0;   
 var bod = document.body;
 var Booly = true;
+var isOpen = true;
+
 
 
 //scroll
@@ -19,13 +24,15 @@ function visibel(){
   topbutton.style.display = "block";
   document.getElementById("head1").style.visibility = "visible";
   saveloc = document.documentElement.scrollTop;   // (اینجا و 2 جا دیگر) برای رفع ارور  
-  document.getElementById("mySidenav").style.top = "90px";
+  document.getElementById("mySidenav").style.top = "90px";  
+  document.getElementById("pointer").style.top = "90px";
   //  document.getElementById("head1").style.background = "none";
 } else {
   topbutton.style.display = "none";
   document.getElementById("head1").style.visibility = "hidden";
   saveloc = document.documentElement.scrollTop; // 2
-  document.getElementById("mySidenav").style.top = 0;
+  document.getElementById("mySidenav").style.top = "0px";
+  document.getElementById("pointer").style.top = "0px";
   //document.getElementById("head1").style.backgroundImage = "linear-gradient(to top,#0553a0,#0884ff)";
 }}       else{
   topbutton.style.display = "none";
@@ -33,32 +40,68 @@ function visibel(){
 }           
 }}
 
-//Scroll To Top _ Button
+// Scroll To Top _ Button
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0;  // For Chrome, Firefox, IE and Opera
  // saveloc = 0;
 }
 
+// close pointer side bar _ Button
+function closepointer(){
+  if(isOpen){
+      isOpen = false;
+      pointerButton.innerHTML = "◀";
+      pointerButton.style.right = "6px";
+  }else{
+      isOpen = true
+      pointerButton.innerHTML = "▶";
+      pointerButton.style.right = "160px";
+  }
+  checksize();
+}
 
 
-//change size   for   Side Bar
+// change size   for   Side Bar  &  pointer
 function checksize(){
-     if(window.innerWidth >= 900){
-     var b = (window.innerWidth) - 235;
-     document.getElementById("mySidenav").style.width = "200px";
-     
-     document.getElementById("bgrand").style.width = b+"px";
+  if(window.innerWidth >= 900){
+    var b = (window.innerWidth) - 235;
 
-     document.getElementById("fot").style.marginLeft = "200px";
-   }
-   
-   else{   
-     document.getElementById("mySidenav").style.width = "0";
-    
-     document.getElementById("bgrand").style.width = "99%";
+    // change size   for   Pointer
+    if(window.innerWidth >= 1200){
+        pointerButton.style.display = "inline-block";
 
-     document.getElementById("fot").style.marginLeft = "0";
+        if(isOpen){
+          document.getElementById("pointer").style.width = "200px";
+          document.getElementById("bgrand").style.marginRight = "200px";
+          document.getElementById("fot").style.marginRight = "200px";
+          b -= 200; }
+          else{
+            document.getElementById("pointer").style.width = "0px";
+            document.getElementById("bgrand").style.marginRight = "0px";
+            document.getElementById("fot").style.marginRight = "0px";
+          }
+
+    } else{
+        pointerButton.style.display = "none";  
+        
+            document.getElementById("pointer").style.width = "0px";
+            document.getElementById("bgrand").style.marginRight = "0px";
+            document.getElementById("fot").style.marginRight = "0px";
+    }
+
+document.getElementById("mySidenav").style.width = "200px";
+document.getElementById("bgrand").style.width = b+"px";
+document.getElementById("fot").style.marginLeft = "200px";
+
+} else{   
+pointerButton.style.display = "none";
+
+document.getElementById("mySidenav").style.width = "0";
+document.getElementById("pointer").style.width = "0px";
+document.getElementById("bgrand").style.width = "99%";
+document.getElementById("bgrand").style.marginRight = "0px";
+document.getElementById("fot").style.margin = "0";
 }}
 
 
@@ -143,9 +186,6 @@ else{
   document.getElementById("ar2").innerHTML = "▶";
 }}
 }
-
-
-
 
 
 
